4537e196740f2a4c71df856239369183ccdf0cf9
Mariana Trinko <marjanavv@gmail.com>
2025-10-02 16:49:56 +0200

    feat(MAIN): project created

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+import { Accordions } from "./components/Accordion/Accordion";
+
+function App() {
+  return (
+    <>
+      <Accordions />
+    </>
+  );
+}
+
+export default App;
